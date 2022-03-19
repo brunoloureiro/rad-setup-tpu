bea664d579aba9e1b03b8d92de29257edb05d191
Fernando Fernandes <fernandofernandesantos@gmail.com>
2022-03-19 20:48:30 +0100

    Timeout exception bug fix.

--- server/machine.py
+++ server/machine.py
@@ -220,11 +220,14 @@ if __name__ == '__main__':
         # FOR DEBUG ONLY
         logger = logging_setup(logger_name="MACHINE_LOG", log_file="unit_test_log_Machine.log")
         logger.debug("DEBUGGING THE MACHINE")
+        server_log_path = "../logs"
+        if os.path.isdir(server_log_path) is False:
+            os.mkdir(server_log_path)
         machine = Machine(
             configuration_file="../machines_cfgs/carolk401.yaml",
             server_ip="131.254.160.174",
             logger_name="MACHINE_LOG",
-            server_log_path="../machines_logs"
+            server_log_path=server_log_path
         )
 
         logger.debug("EXECUTING THE MACHINE")
